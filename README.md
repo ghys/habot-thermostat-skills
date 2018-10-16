3c570973bc64adc7e1c9536c5e30176c8390efe5
Yannick Schaus <github@schaus.net>
2018-10-16 19:33:13 +0200

    Implement IncreaseTemperatureSkill
    Update the README.
    Fix the Maven build.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Example Skills for HABot
 
 This demonstrates how to provide new skills for HABot by writing OSGi components implementing the Skill interface.
- 
+
+It also shows how to use the Eclipse SmartHome semantics to retrieve items.
+
+Make sure HABot is installed and drop it into your openHAB addons folder.
+
+It is meant to be used with the items from the demo package.
+
+For example, say:
+
+- _I'm cold_
+- _It's cold in here_
+
+It will try to identify an (unique) item which is a setpoint related to the Temperature property, and increase its value by 1.
+
+However, if you include a location, like this:
+
+- _It's too cold in the kitchen_
+- _I'm in the living room and I'm cold_
+
+it will try to identify an HVAC equipment in that location instead, and turn it on.
+
+It will also use the CardBuilder to show the relevant card containing the item that has been acted upon.
--- src/main/java/org/openhab/ui/habot/nlp/skill/thermostat/internal/IncreaseTemperatureSkill.java
+++ src/main/java/org/openhab/ui/habot/nlp/skill/thermostat/internal/IncreaseTemperatureSkill.java
@@ -1,16 +1,38 @@
 package org.openhab.ui.habot.nlp.skill.thermostat.internal;
 
 import java.io.InputStream;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
+import org.eclipse.smarthome.core.events.EventPublisher;
+import org.eclipse.smarthome.core.items.Item;
+import org.eclipse.smarthome.core.items.ItemRegistry;
+import org.eclipse.smarthome.core.items.events.ItemEventFactory;
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.semantics.SemanticsPredicates;
+import org.eclipse.smarthome.core.semantics.SemanticsService;
+import org.eclipse.smarthome.core.semantics.model.equipment.HVAC;
+import org.eclipse.smarthome.core.semantics.model.point.Setpoint;
+import org.eclipse.smarthome.core.semantics.model.property.Temperature;
+import org.openhab.ui.habot.card.CardBuilder;
 import org.openhab.ui.habot.nlp.Intent;
 import org.openhab.ui.habot.nlp.IntentInterpretation;
 import org.openhab.ui.habot.nlp.Skill;
 import org.openhab.ui.habot.nlp.UnsupportedLanguageException;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(service = Skill.class, immediate = true)
 public class IncreaseTemperatureSkill implements Skill {
+
+    private ItemRegistry itemRegistry;
+    private SemanticsService semanticsService;
+    private CardBuilder cardBuilder;
+    private EventPublisher eventPublisher;
 
     @Override
     public String getIntentId() {
@@ -37,9 +59,88 @@ public class IncreaseTemperatureSkill implements Skill {
     @Override
     public IntentInterpretation interpret(Intent intent, String language) {
         IntentInterpretation interpretation = new IntentInterpretation();
-        interpretation.setAnswer("Okay, I will increase the temperature");
+
+        String location = intent.getEntities().get("location");
+        if (location != null) {
+            Set<Item> items = semanticsService.getItemsInLocation(location, Locale.ROOT).stream()
+                    .filter(SemanticsPredicates.isA(HVAC.class)).collect(Collectors.toSet());
+            if (items.isEmpty()) {
+                interpretation
+                        .setAnswer("Sorry, I didn't find anything to increase the temperature in the " + location);
+            } else if (items.size() > 1) {
+                interpretation.setAnswer("Weird, I found more than 1 HVAC item in the " + location);
+            } else {
+                eventPublisher
+                        .post(ItemEventFactory.createCommandEvent(items.iterator().next().getName(), OnOffType.ON));
+                interpretation.setAnswer("Okay, I'm turning on the heating in the " + location
+                        + ", this should increase the temperature!");
+                interpretation.setCard(cardBuilder.buildCard(intent, items));
+            }
+        } else {
+            Set<Item> setpoints = itemRegistry.getAll().stream().filter(
+                    SemanticsPredicates.isA(Setpoint.class).and(SemanticsPredicates.relatesTo(Temperature.class)))
+                    .collect(Collectors.toSet());
+
+            if (setpoints.isEmpty()) {
+                interpretation.setAnswer("Sorry, I didn't find any temperature setpoint in your items");
+            } else if (setpoints.size() > 1) {
+                interpretation.setAnswer("I found more than 1 temperature setpoint... I don't know what to do!");
+            } else {
+                Item setpoint = setpoints.iterator().next();
+                Double currentSetpoint = new Double(Double.parseDouble(setpoint.getState().toString()) + 1);
+                eventPublisher.post(ItemEventFactory.createCommandEvent(setpoint.getName(),
+                        setpoint.getAcceptedCommandTypes().contains(DecimalType.class)
+                                ? DecimalType.valueOf(currentSetpoint.toString())
+                                : PercentType.valueOf(currentSetpoint.toString())));
+
+                interpretation.setAnswer(
+                        "Okay, I'm increasing the temperature setpoint a little, it should get warmer soon!");
+                interpretation.setCard(cardBuilder.buildCard(intent, setpoints));
+            }
+        }
 
         return interpretation;
     }
 
+    ///////////////
+
+    @Reference
+    protected void setItemRegistry(ItemRegistry itemRegistry) {
+        if (this.itemRegistry == null) {
+            this.itemRegistry = itemRegistry;
+        }
+    }
+
+    protected void unsetItemRegistry(ItemRegistry itemRegistry) {
+        if (itemRegistry == this.itemRegistry) {
+            this.itemRegistry = null;
+        }
+    }
+
+    @Reference
+    public void setSemanticsService(SemanticsService semanticsService) {
+        this.semanticsService = semanticsService;
+    }
+
+    public void unsetSemanticsService(SemanticsService semanticsService) {
+        this.semanticsService = null;
+    }
+
+    @Reference
+    protected void setCardBuilder(CardBuilder cardBuilder) {
+        this.cardBuilder = cardBuilder;
+    }
+
+    protected void unsetCardBuilder(CardBuilder cardBuilder) {
+        this.cardBuilder = null;
+    }
+
+    @Reference
+    protected void setEventPublisher(EventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    protected void unsetEventPublisher(EventPublisher eventPublisher) {
+        this.eventPublisher = null;
+    }
 }
